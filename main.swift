abc57e8f8a097ca5cc9ae8cae27ca09b6ff82f9c
ilikebanannas <alqamali03@gmail.com>
2020-09-23 01:12:05 +0000

    line 93

--- main.swift
+++ main.swift
@@ -56,23 +56,41 @@ if dollars == 0 {
 // You want your fitness tracking app to give as much encouragement as possible to your users. Create a variable steps equal to the number of steps you guess you've taken today. Create a constant stepGoal equal to 10,000. Write an if-else statement that will print "You're almost halfway there!" if steps is less than half of stepGoal, and will print "You're over halfway there!" if steps is greater than half of stepGoal.
 let stepGoal = 10000
 var steps = 3500
+if steps < (stepGoal / 2) {
+  print("You're almost half way there!")
+} else {
+  print("You're over halfway there!")
+}
 // Now create a new, but similar, if-else-if statement that prints "Way to get a good start today!" if steps is less than a tenth of stepGoal, prints "You're almost halfway there!" if steps is less than half of stepGoal, and prints "You're over halfway there!" if steps is greater than half of stepGoal.
-
+if steps < (stepGoal / 10) {
+  print("Way to get a good start today!")
+} else if steps < (stepGoal / 2) {
+  print("You're almost halfway there!")
+} else {
+  print("You're over half way there!")
+}
 /* Imagine you're going to dinner with friends and are struggling to decide where to go. Two of you have very strong opinions and have clearly laid out your requirements for dinner as follows:
 
 You want to eat somewhere that has either fish or pizza
 Your friend wants to eat somewhere with vegan options.
 
 Another friend brings up a restaurant she thinks will fit both of your criteria. This restaurant's attributes are represented by a few constants below. Write an if-else statement that will print "Let's go!" if the restaurant's attributes match the group's dietary requirements, and otherwise will print "Sorry, we'll have to think of somewhere else." */
-let hasFish = true
+var hasFish = true
 let hasPizza = false
 let hasVegan = true
-
+if ((hasFish == true) || (hasPizza == true)) && hasVegan == true {
+  print("Let's go!")
+} else {
+  print("Sorry, we'll have to think of somewhere else")
+}
 // Imagine you're trying to decide whether or not to go on a walk. You decide that you'll go on a walk if it's not raining or if it's 82 degress or warmer and sunny out. Create a constant isNiceWeather that is equal to an expression that evaluates to a boolean indicating whether or not the weather is nice enough for you to go for a walk. Write an if statement that will print "I'm going for a walk!" if the weather is nice.
 let temp = 82
 let isRaining = true
 let isSunny = true
-
+let isNiceWeather = ((temp >= 82) && (isSunny == true)) || isRaining == false
+if isNiceWeather == true {
+  print("I'm going for a walk!")
+}
 /* You decide that you want your fitness tracker to have a feature that helps users stay inside specified heart rate zones while they are working out. You'll display a message to the user telling them to go a little faster to increase their heart rate if they are below the target, tell them that they are spot on if they are in the target, and tell them to slow it down a little if they are over the target.
 
 Create constants isInTarget, isBelowTarget, and isAboveTarget that equal expressions that evaluate to whether or not currentHR is between the lower and upper bounds, below the lower bound, and above the upper bound, respectively. Then write an if-else-if statement that will print "You're right on track!" if the user is inside the target zone, "You're doing great, but try to push it a bit!" if the user is below the target zone, and "You're on fire! Slow it down just a bit." if the user is above the target zone. */
