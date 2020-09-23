9a4a43eae9f869372dd8eafdc02e770baa93f533
ilikebanannas <alqamali03@gmail.com>
2020-09-23 00:41:00 +0000

    Bank account section completed (line 54)

--- main.swift
+++ main.swift
@@ -33,12 +33,25 @@ print(false)
 print(!true)
 // Imagine you're creating a machine that will count your money for you and tell you how wealthy you are based on how much money you have. A variable dollars has been given to you with a value of 0. Write an if statement that prints "Sorry, kid. You're broke!" if dollars has a value of 0. Observe what is printed to the console.
 var dollars = 0
-
+if dollars == 0 {
+  print("Sorry, kid. You're broke!")
+}
 // dollars has been updated below to have a value of 10. Write an an if-else statement that prints "Sorry, kid. You're broke!" if dollars has a value of 0, but prints "You've got some spending money!" otherwise. Observe what is printed to the console.
 dollars = 10
-
+if dollars == 0 {
+  print("Sorry, kid. You're broke!")
+} else {
+  print("You've got some spending money!")
+}
 // dollars has been updated below to have a value of 105. Write an an if-else-if statement that prints "Sorry, kid. You're broke!" if dollars has a value of 0, prints "You've got some spending money!" if dollars is less than 100, and prints "Looks to me like you're rich!" otherwise. Observe what is printed to the console.
 dollars = 105
+if dollars == 0 {
+  print("Sorry, kid. You're broke!")
+} else if dollars < 100 {
+  print("You've got some spending money!")
+} else {
+  print("Looks to me like you're rich!")
+}
 
 // You want your fitness tracking app to give as much encouragement as possible to your users. Create a variable steps equal to the number of steps you guess you've taken today. Create a constant stepGoal equal to 10,000. Write an if-else statement that will print "You're almost halfway there!" if steps is less than half of stepGoal, and will print "You're over halfway there!" if steps is greater than half of stepGoal.
 let stepGoal = 10000
